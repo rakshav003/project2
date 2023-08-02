4fdf1e4e72933dfa0d8215c2c8aa53faa64f7eb1
Rakshav Patel <123899697+rakshav003@users.noreply.github.com>
2023-08-02 17:35:53 -0500

    updated

--- project2.py
+++ project2.py
@@ -22,16 +22,16 @@ def create_customer(cId,Cname,Street,City,StateAb,Zipcode):
     return f"CREATE (:CUSTOMER{{cId: '{cId}', Cname: '{Cname}', Street: '{Street}', City: '{City}', StateAb: '{StateAb}', Zipcode: '{Zipcode}'}})"
 
 def create_store(sId,Sname,Street,City,StateAb,ZipCode,Sdate,Telno,URL):
-    return f"CREATE(:STORE{{sId: '{sId}', Sname: '{Sname}', Street: '{Street}' City: '{City}', StateAb: '{StateAb}', ZipCode '{ZipCode}', Sdate: '{Sdate}', Telno: '{Telno}', URL: '{URL}'}})"
+    return f"CREATE(:STORE{{sId: '{sId}', Sname: '{Sname}', Street: '{Street}', City: '{City}', StateAb: '{StateAb}', ZipCode: '{ZipCode}', Sdate: '{Sdate}', Telno: '{Telno}', URL: '{URL}'}})"
 
-def create_order_item(oId,iId,Icount):
-    return f"CREATE (:ORDER_ITEM{{oId: '{oId}', iId: '{iId}', Icount: '{Icount}'}})"
+def create_order_item(oId, iId, Icount):
+    return f"CREATE(:ORDER_ITEM{{oId: '{oId}', iId: '{iId}', Icount: '{Icount}'}})"
 
-def create_oldprice(iId,Sprice,Sdate,Edate):
+def create_oldprice(iId, Sprice, Sdate, Edate):
     return f"CREATE(:OLDPRICE{{iId: '{iId}', Sprice: '{Sprice}', Sdate: '{Sdate}', Edate: '{Edate}'}})"
 
 def create_employee(sId,SSN,Sname,Street,City,StateAb,Zipcode,Etype,Bdate,Sdate,Edate,Level,Asalary,Agency,Hsalary,Institute,Itype):
-    return f"CREATE(:EMPLOYEE{{sId: '{sId}', SSN: '{SSN}' Sname: '{Sname}', Street: '{Street}', City: '{City}', Bdate: '{Bdate}', StateAb: '{StateAb}', Zipcode: '{Zipcode}', Etype: '{Etype}', Sdate: '{Sdate}', Edate: '{Edate}', Level: '{Level}', Asalary: '{Asalary}', Agency: '{Agency}', Hsalary: '{Hsalary}', Institute: '{Institute}', Itype: '{Itype}'}})"
+    return f"CREATE(:EMPLOYEE{{sId: '{sId}', SSN: '{SSN}', Sname: '{Sname}', Street: '{Street}', City: '{City}', Bdate: '{Bdate}', StateAb: '{StateAb}', Zipcode: '{Zipcode}', Etype: '{Etype}', Sdate: '{Sdate}', Edate: '{Edate}', Level: '{Level}', Asalary: '{Asalary}', Agency: '{Agency}', Hsalary: '{Hsalary}', Institute: '{Institute}', Itype: '{Itype}'}})"
 
 def create_contract(vId,ctId,Sdate,Ctime,Cname):
     return f"CREATE(:CONTRACT{{vId: '{vId}', ctId: '{ctId}', Sdate: '{Sdate}', Ctime: '{Ctime}', Cname: '{Cname}'}})"
@@ -89,8 +89,8 @@ with driver.session(database="neo4j") as session:
     with open("Sprouts Data/ORDER_ITEM.csv") as order_item_file:
         next(order_item_file)
         for line in order_item_file:
-            oId, iId, Icoun = line.strip().split(',')
-            query = create_order_item(oId, iId, Icoun)
+            oId, iId, Icount = line.strip().split(',')
+            query = create_order_item(oId, iId, Icount)
             session.run(query)     
 
     with open("Sprouts Data/OLDPRICE.csv") as olderprice_file:
@@ -114,6 +114,7 @@ with driver.session(database="neo4j") as session:
             query = create_contract(vId,ctId,Sdate,Ctime,Cname)
             session.run(query)
 
+    
     # VENDOR SELLS ITEM 
     sells_query = '''
     MATCH (v:VENDOR), (vi:VENDOR_ITEM), (i:ITEM)
@@ -138,28 +139,29 @@ with driver.session(database="neo4j") as session:
     '''
     session.run(belongsto_query)
     
-    # # ORDER CONTAINS ITEM 
-    # contains_query = '''
-    # MATCH (o:ORDERS), (oi:ORDER_ITEM), (i:ITEM)
-    # WHERE o.oId = oi.oId AND oi.iId = i.iId
-    # MERGE (o)-[:CONTAINS]->(i)
-    # '''
-    # session.run(contains_query)
-
+    # ORDER CONTAINS ITEM 
+    contains_query = '''
+    MATCH (o:ORDERS), (oi:ORDER_ITEM), (i:ITEM)
+    WHERE o.oId = oi.oId AND oi.iId = i.iId
+    MERGE (o)-[:CONTAINS]->(i)
+    '''
+    session.run(contains_query)
+    
     #order_item MATCHES item
     matches_query = '''
-    MATCH (oi: ORDER_ITEM), (i:ITEM)
-    WHERE oi.iID = i.iId
-    MERGE (oi)-[:MATHES]->(i)
+    MATCH (oi:ORDER_ITEM), (i:ITEM)
+    WHERE oi.iId = i.iId
+    MERGE (oi)-[:MATCHES]->(i)
     '''
     session.run(matches_query)
-
+    
     #Employee WORKS_AT Store
     works_query = '''
-    MATCH (e: EMPLOYEE), (s: STORE)
+    MATCH (e:EMPLOYEE), (s:STORE)
     WHERE e.sId = s.sId
     MERGE (e)-[:WORKS_AT]->(s)
     '''
     session.run(works_query)
+       
 
 driver.close()
